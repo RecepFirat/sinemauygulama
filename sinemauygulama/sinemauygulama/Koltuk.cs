31424b820d29eb6be3d56ee65307facfd13fb80c
recep tayyip fırat <rtf3444@gmail.com>
2018-01-26 01:59:46 +0300

    26.01.2018 01:59 koltuk button classı kaldı ordan devam et

---
+++ sinemauygulama/sinemauygulama/Koltuk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sinemauygulama
+{//35.dakıkadayım
+    class Koltuk:Button//sen bi buttonsun dıyorum senın ıcıne buttonlar olucak
+    {
+
+
+        public int Numara { get; set; }
+        public string Sıra { get; set; }
+
+        public Bilet Bilet { get; set; }
+
+        public KoltukDurum Durum { get; set; }
+    }
+
+    enum KoltukDurum {
+        Bos,
+        Satildi,
+        Rezerve,
+        Kapali
+
+    }
+}
---
+++ sinemauygulama/sinemauygulama/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinemauygulama
+{
+    class Musteri
+    {
+        public String Adi { get; set; }
+        public string Telefon { get; set; }
+
+
+    }
+}
