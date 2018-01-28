2ca706f30ccb9167697a6ca206a14c56c6cfe2d3
recep tayyip fırat <rtf3444@gmail.com>
2018-01-28 05:01:09 +0300

    28.01.2018

--- sinemauygulama/sinemauygulama/Form1.Designer.cs
+++ sinemauygulama/sinemauygulama/Form1.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.koltuk1 = new sinemauygulama.Koltuk();
             this.SuspendLayout();
+            // 
+            // koltuk1
+            // 
+            this.koltuk1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("koltuk1.BackgroundImage")));
+            this.koltuk1.Bilet = null;
+            this.koltuk1.Durum = sinemauygulama.KoltukDurum.Bos;
+            this.koltuk1.Location = new System.Drawing.Point(13, 25);
+            this.koltuk1.Name = "koltuk1";
+            this.koltuk1.Numara = 0;
+            this.koltuk1.Sıra = null;
+            this.koltuk1.Size = new System.Drawing.Size(50, 49);
+            this.koltuk1.TabIndex = 0;
+            this.koltuk1.Text = "koltuk1";
+            this.koltuk1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.koltuk1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +60,8 @@
         }
 
         #endregion
+
+        private Koltuk koltuk1;
     }
 }
 
--- sinemauygulama/sinemauygulama/Koltuk.cs
+++ sinemauygulama/sinemauygulama/Koltuk.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -7,11 +8,12 @@ using System.Windows.Forms;
 namespace sinemauygulama
 {//35.dakıkadayım
     class Koltuk:Button//sen bi buttonsun dıyorum senın ıcıne buttonlar olucak
-    {
+    {//bir nevi kendim user control yaptım
         public Koltuk()
         {
             Width = 50;
             Height = 50;
+            BackgroundImage =new Bitmap( Properties.Resources.koltuk1,50,50);//boyutunu 50 50 ayarladım
         }
 
         public int Numara { get; set; }
